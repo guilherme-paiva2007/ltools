fe7a28304996b36c1d856cc868016e1090417895
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2024-11-21 07:44:33 -0300

    a

--- js/web_script.js
+++ js/web_script.js
@@ -1,11 +1,23 @@
 // Storage Management
 
+class WebStorageManager {
+    constructor() {}
+}
 
+// Visual Control
+
+class PageTheme {}
 
 // Interactive Elements
 
+class DynamicSearch {
+    constructor(input, searchLink) {}
 
+    static Button = class DynamicSearchButton {} // botao de limpar, basiquinho
+
+    static Fetch = class DynamicSearchFetch {} // como q eu salvo um padrão de elementos em js...
+}
 
 if (typeof module !== "undefined") {
-    module.exports
+    module.exports = {}
 }
